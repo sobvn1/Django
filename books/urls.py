6f13e33b32300bd45bd0b0e941f3546e2dea7968
sobvn <isken00700@gmail.com>
2022-12-21 00:12:38 +0600

    books

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.books_views, name='tv_show'),
+    path('books/', views.books_views, name='books'),
     path('books_detail/<int:id>/', views.books_detail, name='detail')
 ]
